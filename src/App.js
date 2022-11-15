241ffdc5934aa13cdcc1b377018e1d96d33cbab6
unknown <peacesandy04@gmail.com>
2022-11-15 09:02:56 -0800

    Adjusted the app.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/places' element={<Placetostay/>} />
-
         Placetostay
       </Routes>
     </div>
